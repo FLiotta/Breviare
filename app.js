1f2e652fff2105496c4923e3b9e72d27bb5e4f41
FLiotta <liottafabrizio@gmail.com>
2018-12-15 02:50:06 -0300

    Gitbuttons, removed server views

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+const methodOverride = require('method-override');
 const publicIp = require('public-ip');
+const path = require('path');
 
 var app = express();
-const port = process.env.PORT || 5000;
-
 const routes = require('./server/routes/routes');
 
 app.use(methodOverride('_method'));
@@ -20,6 +19,17 @@ app.use((req,res,next) => {
 });
 
 app.use('/api', routes);
+
+if(process.env.NODE_ENV === "production"){
+	app.use(express.static('client/build'));
+
+	app.get('*', (req,res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	})
+}
+
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
--- client/src/Main/Header/Header.js
+++ client/src/Main/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import GitButtons from '../../GitButtons';
 
 export default () => (
-	<div className="row justify-content-md-center" style={{paddingBottom: "5%"}}>
+	<div className="row justify-content-md-center" style={{paddingBottom: "0%"}}>
         <div className="col-md-6">
             <img src="/img/logo2.jpg" alt="logo" className="img-fluid mx-auto d-block animated fadeInDown"/>
+            <br/>
+        <GitButtons />
         </div>
     </div>
 );
